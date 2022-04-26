d5a59c6327e072a30103c8469541759fa5f0c870
Ahmad Ridwan Tresna Nugraha <47095750+artnugraha@users.noreply.github.com>
2022-04-27 05:29:37 +0700

    Update install script

--- QEinstall.sh
+++ QEinstall.sh
@@ -6,14 +6,14 @@ sudo apt -y upgrade
 
 # Important development tools and libraries:
 # Git, wget, gcc/g++/gfortran, LAPACK, FFTW, openmpi (mpirun)
-sudo apt install git wget build-essential g++ gfortran liblapack-dev libfftw3-dev libopenmpi-dev
+sudo apt -y install git wget build-essential g++ gfortran liblapack-dev libfftw3-dev libopenmpi-dev
 
 # Quantum ESPRESSO and Wannier90
-sudo apt install quantum-espresso wannier90
+sudo apt -y install quantum-espresso wannier90
 
 # Additional tools
-sudo apt install xcrysden gnuplot
-sudo apt install python3-dev python3-pip
+sudo apt -y install xcrysden gnuplot
+sudo apt -y install python3-dev python3-pip
 pip3 install numpy scipy sympy matplotlib jupyterlab
 
 # Completion message
